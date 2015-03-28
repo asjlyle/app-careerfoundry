83e9f81e82e4ed5005cc5e93ec91d5e8554980ad
Annabel <a.sjlyle@gmail.com>
2015-03-28 21:19:11 +0000

    add mailer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,3 +6,13 @@ class StaticPagesController < ApplicationController
   	@products=Product.limit(3)
   end
 end
+
+def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  ActionMailer::Base.mail(:from => @email, 
+	  	:to => 'a.sjlyle@gmail.com', 
+	  	:subject => "A new contact form message from #{@name}", 
+	  	:body => @message).deliver
+end
