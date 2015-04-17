a4c0f14842453dfb6edb15121bea3fe98513f045
Annabel <a.sjlyle@gmail.com>
2015-04-17 16:05:58 +0100

    add jquery stars

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,5 @@
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
-$(document).on('ready page:load', function(){
-	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' })
-	$('.rated').raty({ path: '/assets', 
-	  readOnly: true,
-	  score: function() {
-	    return $(this).attr('data-score');
-	  }
-	});
-});
-
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
 // or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
@@ -25,3 +15,13 @@ $(document).on('ready page:load', function(){
 //= require turbolinks
 //= require_tree .
 
+
+$(document).on('ready page:load', function(){
+	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' })
+	$('.rated').raty({ path: '/assets', 
+	  readOnly: true,
+	  score: function() {
+	    return $(this).attr('data-score');
+	  }
+	});
+});
